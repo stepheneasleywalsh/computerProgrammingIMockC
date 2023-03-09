25bcabade97aaa46552990b55de4e5a264e030e9
Stephen Easley-Walsh <stephen.easley-walsh@studygroup.com>
2023-03-09 16:31:15 +0000

    Update main.py
    Better Code

--- main.py
+++ main.py
@@ -1,77 +1,64 @@
-# Student Number
+# We need to import random to generate the questions
 import random
 
-# This sets the score to 0 and the number of lives to 3
+# Give the player 3 lives and score AND how many questions asked
 lives = 3
 score = 0
+ask = 0
 
-
-# This is a for loop, at most ask 10 questions but game over if 3 are answered wrongly
-for questionNumber in range(1,11):
-    # Prints the question number
-    print("Question", questionNumber)
-    # Chooses a random question, 1 from 4 types
-    questionType = random.randint(1,4)
-    # If they have no life left, game over and BREAK OUT OF FOR
-    if lives <= 0:
-        print("OUT OF LIFE! GAME OVER!")
+# Loop for the game
+while True:
+    # Check if they are finished
+    if lives == 0:
+        print("GAME OVER")
         break
-    # + question
-    elif questionType == 1:
+    elif ask == 10:
+        print("YOU WIN!")
+        print("You scored", score, "out of 10")
+        break
+    # The program picks a random number from 1 to 4, this will determine the type of
+    # question to ask the user
+    typeOfQuestion = random.randint(1,4)
+    ask += 1
+    print("Question", ask)
+    # ASK one of the four types of questions
+    if typeOfQuestion == 1:
+        # Addition Question
         x = random.randint(1,12)
         y = random.randint(1,12)
-        rightAnswer = x + y
-        print("What is", x, "+", y, "?")
+        theAnswer = x + y
+        print("What is",x,"+",y,"?")
         theirAnswer = int(input("> "))
-        if theirAnswer == rightAnswer:
-            print("CORRECT!")
-            score += 1
-        else:
-            print("WRONG!")
-            lives -= 1
-    # - question
-    elif questionType == 2:
-        x = random.randint(6,12)
-        y = random.randint(1,5)
-        rightAnswer = x - y
-        print("What is", x, "-", y, "?")
-        theirAnswer = int(input("> "))
-        if theirAnswer == rightAnswer:
-            print("CORRECT!")
-            score += 1
-        else:
-            print("WRONG!")
-            lives -= 1
-    # * question
-    elif questionType == 3:
+    elif typeOfQuestion == 2:
+        # Subtraction
         x = random.randint(1,12)
         y = random.randint(1,12)
-        rightAnswer = x * y
-        print("What is", x, "*", y, "?")
+        theAnswer = x - y
+        print("What is",x,"-",y,"?")
         theirAnswer = int(input("> "))
-        if theirAnswer == rightAnswer:
-            print("CORRECT!")
-            score += 1
-        else:
-            print("WRONG!")
-            lives -= 1
-    # / question
-    elif questionType == 4:
-        k = random.randint(1, 12)
-        y = random.randint(1, 12)
-        x = k * y
-        rightAnswer = x / y
-        print("What is", x, "/", y, "?")
+    elif typeOfQuestion == 3:
+        # Multiplication
+        x = random.randint(1,12)
+        y = random.randint(1,12)
+        theAnswer = x * y
+        print("What is",x,"*",y,"?")
         theirAnswer = int(input("> "))
-        if theirAnswer == rightAnswer:
-            print("CORRECT!")
-            score += 1
-        else:
-            print("WRONG!")
-            lives -= 1
+    elif typeOfQuestion == 4:
+        # Division
+        x = random.randint(1,12)
+        y = random.randint(1,12)
+        z = x*y
+        y = z/x
+        theAnswer = y
+        print("What is ",z,"/",x,"?")
+        theirAnswer = int(input("> "))
+    # Check if they are right or wrong
+    if theAnswer == theirAnswer:
+        print("CORRECT!")
+        score += 1
+    else:
+        print("WRONG!")
+        lives -= 1
 
-# Summary of score
-print("Your score is", score, "out of 10.")
-
-# Quit
+# Quit the program
 quit()
